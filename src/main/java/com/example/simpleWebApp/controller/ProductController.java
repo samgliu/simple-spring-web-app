4edbcfcce1f97cecd72937e38b99d07595ae73ff
Sam Liu <samgliu19@gmail.com>
2026-06-28 16:57:06 -0400

    chore: test post get put delete

--- src/main/java/com/example/simpleWebApp/controller/ProductController.java
+++ src/main/java/com/example/simpleWebApp/controller/ProductController.java
@@ -3,7 +3,12 @@ package com.example.simpleWebApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.simpleWebApp.model.Product;
@@ -15,8 +20,28 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts() {
         return service.getProducts();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product geProduct(@PathVariable int prodId) {
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod) {
+        service.addProduct(prod);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod) {
+        service.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void updateProduct(@PathVariable int prodId) {
+        service.deleteProduct(prodId);
     }
 }
--- src/main/java/com/example/simpleWebApp/service/ProductService.java
+++ src/main/java/com/example/simpleWebApp/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.simpleWebApp.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,11 +11,45 @@ import com.example.simpleWebApp.model.Product;
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(new Product(101, "Iphone", 50000),
-            new Product(102, "Canon Camera", 70000));
+    List<Product> products = new ArrayList<>(Arrays.asList(
+            new Product(101, "Iphone", 50000),
+            new Product(102, "Canon Camera", 70000),
+            new Product(103, "Hydroflask", 70)));
 
     public List<Product> getProducts() {
         return products;
+    }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst().get();
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        int index = 0;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProdId() == prod.getProdId()) {
+                index = i;
+                break;
+            }
+        }
+        products.set(index, prod);
+    }
+
+    public void deleteProduct(int prodId) {
+        int index = 0;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProdId() == prodId) {
+                index = i;
+                break;
+            }
+        }
+        products.remove(index);
     }
 
 }
